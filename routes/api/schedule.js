7af0063308a295d1f72b964274e60bea70d8b515
Bhushan Lal <bhushanlal972@gmail.com>
2019-10-29 16:19:43 +0530

    Added jwt auth for schedule api

--- routes/api/schedule.js
+++ routes/api/schedule.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const validateToken = require("../validateToken").validateToken;
 // Load input validation
 // const validateLoginInput = require("../../validation/login");
 // Load User model
@@ -11,7 +12,7 @@ const Schedule = require("../../models/Schedule");
 // @route POST api/schedule
 // @desc Create Schedule api
 // @access Private
-router.post("/add", (req, res) => {
+router.post("/add", validateToken, (req, res) => {
   // return res.json(req.body);
   const {campaign, type, start_date, end_date, day_limit, start_time, days, time_zone, message, phone_number, fail_message} = req.body;
   var scheduleData = new Schedule({
