dceb9d4f2967ac2b76d1b46632348920f6437f7f
Aaron Busary <aaron@innsite.com.au>
2019-10-29 17:42:37 +1100

    updates to admin theme

--- client/src/components/layout/TopBar/UserMenu/index.js
+++ client/src/components/layout/TopBar/UserMenu/index.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Menu, Dropdown, Avatar, Badge } from 'antd'
+import { Menu, Dropdown, Avatar } from 'antd'
 import styles from './style.module.scss'
 
 @connect(({ user }) => ({ user }))
 class ProfileMenu extends React.Component {
-  state = {
-    count: 7,
-  }
-
   logout = () => {
     const { dispatch } = this.props
     dispatch({
@@ -16,17 +12,8 @@ class ProfileMenu extends React.Component {
     })
   }
 
-  addCount = () => {
-    let { count } = this.state
-    count += 1
-    this.setState({
-      count,
-    })
-  }
-
   render() {
     const { user } = this.props
-    const { count } = this.state
     const menu = (
       <Menu selectable={false}>
         <Menu.Item>
@@ -47,7 +34,7 @@ class ProfileMenu extends React.Component {
             {user.email}
             <br />
             <strong>Phone: </strong>
-            {user.phone || '— '}
+            {user.phone}
           </div>
         </Menu.Item>
         <Menu.Divider />
@@ -67,11 +54,9 @@ class ProfileMenu extends React.Component {
       </Menu>
     )
     return (
-      <Dropdown overlay={menu} trigger={['click']} onVisibleChange={this.addCount}>
+      <Dropdown overlay={menu} trigger={['click']}>
         <div className={styles.dropdown}>
-          <Badge count={count}>
-            <Avatar className={styles.avatar} shape="square" size="large" icon="user" />
-          </Badge>
+          <Avatar className={styles.avatar} shape="square" size="large" icon="user" />
         </div>
       </Dropdown>
     )
